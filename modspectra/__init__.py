525894e5e2960108f65be7676ee4909dfd5c8d9c
DK <dhanesh.krishnarao@icloud.com>
2019-01-21 16:09:48 -0600

    update .travis
    remove main_cmd as a test

--- modspectra/__init__.py
+++ modspectra/__init__.py
@@ -20,7 +20,6 @@ if sys.version_info < tuple((int(val) for val in __minimum_python_version__.spli
 
 if not _ASTROPY_SETUP_:
     # For egg_info test builds to pass, put package imports here.
-    from .example_mod import *
     from .cube import *
     from .cubeMixin import *
     
